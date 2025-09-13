3f9de2620f3410953a3fbe1fd6755d43127646d0
Juanmanueloddone <juancho12oddone@gmail.com>
2025-09-13 10:35:44 -0300

    Update version.py

--- api/version.py
+++ api/version.py
@@ -1,12 +1,2 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-class VersionInfo(BaseModel):
-    service: str
-    version: str
-
-app = FastAPI()
-
-@app.get("/", response_model=VersionInfo)
-def version():
-    return VersionInfo(service="inmoflow-ml", version="0.1.0")
+def handler(request):
+    return {"service": "inmoflow-ml", "version": "0.1.0"}
