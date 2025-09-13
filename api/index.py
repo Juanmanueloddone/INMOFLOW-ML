e37a21841f713f7fe4820eb5af1e7250c405ab1c
Juanmanueloddone <juancho12oddone@gmail.com>
2025-09-13 13:58:09 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,10 +3,10 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/api/health")
+@app.get("/health")
 async def health():
     return JSONResponse({"status": "ok"})
 
-@app.get("/api/version")
+@app.get("/version")
 async def version():
     return JSONResponse({"version": "1.0.0"})
